2d223ec257dfe0d99c800533cc55b0aef59338d3
angpace <108838753+angpace@users.noreply.github.com>
2022-10-31 13:37:05 -0400

    commit tree bb7bfd4606aa7e72bcfeccc3deb867cce1844067

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def my_method(param)
+    puts "Running my_method"
+    param + 1
+  end
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (a, b)
+    a + b 
+end
+
+def halve (a)
+ a % 2
+end 
+
